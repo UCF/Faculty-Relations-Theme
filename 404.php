eb4a09f22fb6d22073c514f2bd26b9a2504893f0
Jo Greybill <jogreybill@gmail.com>
2013-06-17 17:28:21 -0400

    Fixed 404 template

--- 404.php
+++ 404.php
@@ -1,30 +1,27 @@
 <?php @header("HTTP/1.1 404 Not found", true, 404);?>
 <?php disallow_direct_load('404.php');?>
-
-<?php get_header(); the_post();?>
-	<div class="row page-content" id="page-not-found">
-		<div class="span9">
-			<article>
-				<h1>Page Not Found</h1>
-				<?php 
+<?php get_header(); ?>
+	<div class="container">
+		<?php the_post();?>
+		<div class="row page-content" id="<?=$post->post_name?>">
+			<div class="span12">
+				<article>
+					<h2>Page Not Found</h2>
+					<?php 
 					$page = get_page_by_title('404');
-					if($page){
-						$content = $page->post_content;
-						$content = apply_filters('the_content', $content);
-						$content = str_replace(']]>', ']]>', $content);
-					}
-				?>
-				<?php if($content):?>
-				<?=$content?>
-				<?php else:?>
-				<p>The page you requested doesn't exist.  Sorry about that.</p>
-				<?php endif;?>
-			</article>
-		</div>
-		
-		<div id="sidebar" class="span3">
-			<?=get_sidebar();?>
+						if($page){
+							$content = $page->post_content;
+							$content = apply_filters('the_content', $content);
+							$content = str_replace(']]>', ']]>', $content);
+						}
+					?>
+					<?php if($content):?>
+					<?=$content?>
+					<?php else:?>
+					<p>The page you requested doesn't exist.  Sorry about that.</p>
+					<?php endif;?>
+				</article>
+			</div>
 		</div>
 	</div>
-	<?php get_template_part('includes/below-the-fold'); ?>
 <?php get_footer();?>
